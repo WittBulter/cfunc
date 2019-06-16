70be8fbdb924e729a71e69c40eeda15ff09e578f
WittBulter <lambdas.dev@gmail.com>
2019-06-16 14:00:50 +0800

    chore: add deprecated message

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import * as commands from './actions'
 import * as options from './options'
 import { Container } from 'func'
+import * as print from './utils/print'
+
+const next = print.cyanColor('func-service')
+console.log(print.dangerColor('> Deprecated.'))
+const text = print.logColor(`> Use package "${next}" instead.`)
+console.log(text)
+console.log('')
 
 const modules = Object.assign({}, commands, options)
 const params = Object.keys(modules).map(key => modules[key])
